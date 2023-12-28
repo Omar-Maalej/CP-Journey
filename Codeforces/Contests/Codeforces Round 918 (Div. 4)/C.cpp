7e75ba3f40606cc13646afe86d3baf7366503541
Omar-Maalej <omar.maalej@insat.ucar.tn>
2023-12-28 23:14:57 +0100

    CF Round 918 C update

--- Codeforces/Contests/Codeforces Round 918 (Div. 4)/C.cpp
+++ Codeforces/Contests/Codeforces Round 918 (Div. 4)/C.cpp
@@ -1,19 +1,21 @@
 #include <bits/stdc++.h>
+#define fastIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 using namespace std;
 
 int main(){
+    fastIO;
     int t;
     cin >> t;
     while(t--){
         int n;
         cin >> n;
-        double sum=0;
+        long long sum=0;
         for(int i=0;i<n;i++){
-            double a;
+            long long a;
             cin >> a;
             sum+=a;
         }
-        double x = floor(sqrt(sum));
+        long long x = floor(sqrt(sum));
         if(x*x==sum)cout << "YES" << endl;
         else cout << "NO" << endl;
 
