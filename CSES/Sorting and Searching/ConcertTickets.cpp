6fee747d0c1f90b6970d1327fa064d6c0498b99c
Omar-Maalej <omar.maalej@insat.ucar.tn>
2024-01-23 19:47:15 +0100

    CSES Sorting ConcertTickets

--- CSES/Sorting and Searching/ConcertTickets.cpp
+++ CSES/Sorting and Searching/ConcertTickets.cpp
@@ -4,24 +4,26 @@ using namespace std;
 int main(){
     int n,m;
     cin >> n >> m;
-    vector<int> h(n,0);
-    vector<int> t(m,0);
+    multiset<int, greater<int>> h;
+    vector<int> t;
     for(int i=0;i<n;i++){
-        cin >> h[i];
-        }
-    for(int i=0;i<m;i++){
-        cin >> t[i];
+        int e;
+        cin >> e;
+        h.insert(e);
     }
-    sort(h.begin(), h.end());
     for(int i=0;i<m;i++){
-        int j=0;
-        cout << t[i] << " : " << endl;
-        while(j<n && h[j]<=t[i] && h[j]!=-1){
-                cout << h[j] << endl;
-                j++;
+        int e;
+        cin >> e;
+        t.push_back(e);
         }
-        cout << h[j-1] << endl;
-        h[j-1]=-1;
+    for(int i=0;i<m;i++){
+        auto x = h.lower_bound(t[i]);
+        if(x!=h.end()){
+            cout << *x << endl;
+            h.erase(h.find(*x));
+        }else{
+            cout << -1 << endl;
+        }
     }
     return 0;
 }
