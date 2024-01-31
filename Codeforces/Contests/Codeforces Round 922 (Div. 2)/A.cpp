1ed71de3b8c518c4bf2b2b07b57e926467008931
Omar-Maalej <omar.maalej@insat.ucar.tn>
2024-01-31 16:35:46 +0100

    CF Round 922

---
+++ Codeforces/Contests/Codeforces Round 922 (Div. 2)/A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        long long n,m;
+        cin >> n >> m;
+        cout << (m/2) * n << endl;
+    }
+    return 0;
+}
